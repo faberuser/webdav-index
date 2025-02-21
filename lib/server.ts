5b34c4185414e1b7170225af9c92fba16ffed8a7
khoile <faber8164@gmail.com>
2025-02-21 17:48:20 +0700

    test include creds

--- lib/server.ts
+++ lib/server.ts
@@ -1,49 +1,61 @@
-import { root_dir, remoteURL, username, password, fuzzy, cacheRefresh } from "@/config"
+import {
+    root_dir,
+    remoteURL,
+    username,
+    password,
+    fuzzy,
+    cacheRefresh,
+} from "@/config"
 import { createClient } from "webdav"
-import NodeCache from 'node-cache'
-import https from 'https'
+import NodeCache from "node-cache"
+import https from "https"
 import sharp from "sharp"
-const fuzz = require('fuzzball')
+const fuzz = require("fuzzball")
 
 const contentsCache = new NodeCache({ stdTTL: cacheRefresh, checkperiod: 120 })
 const agent = new https.Agent({ rejectUnauthorized: false })
-
 
 const client = createClient(remoteURL, {
     username: username,
     password: password,
     httpsAgent: agent,
+    withCredentials: true,
 })
 
-
 function generateRandomString(length: number, parts: number) {
-    let result = ''
-    const characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    let result = ""
+    const characters = "abcdefghijklmnopqrstuvwxyz0123456789"
     const segmentLength = Math.floor(length / parts)
     for (let i = 0; i < length; i++) {
         if (i > 0 && i % segmentLength === 0 && result.length < length - 1) {
-            result += '-'
+            result += "-"
         } else {
-            result += characters.charAt(Math.floor(Math.random() * characters.length))
+            result += characters.charAt(
+                Math.floor(Math.random() * characters.length)
+            )
         }
     }
     return result
 }
 
-
 export async function getContentsCache() {
     let dirContents: any = contentsCache.get("contents")
 
     if (!dirContents) {
-        dirContents = await client.getDirectoryContents(root_dir, { deep: true })
+        dirContents = await client.getDirectoryContents(root_dir, {
+            deep: true,
+        })
         contentsCache.set("contents", dirContents)
     }
 
     return dirContents
 }
 
-
-export async function listContents(dir: string, deep: boolean = false, countItems: boolean = false) {
+export async function listContents(
+    dir: string,
+    deep: boolean = false,
+    countItems: boolean = false
+) {
     let dirContents: any = await getContentsCache()
 
     // dirContents = dirContents.map(obj => {
@@ -67,33 +79,44 @@ export async function listContents(dir: string, deep: boolean = false, countItem
     if (deep) {
         return dirContents
             .filter((_dir: any) => {
-                return _dir.filename.startsWith(fullPath + '/')
+                return _dir.filename.startsWith(fullPath + "/")
             })
             .map((_dir: any) => {
                 return {
                     ..._dir,
-                    filename: _dir.filename.replace(root_dir, '')
+                    filename: _dir.filename.replace(root_dir, ""),
                 }
             })
-
     } else {
-        const partsLength = fullPath.split('/').length
+        const partsLength = fullPath.split("/").length
 
         return dirContents
             .filter((_dir: any) => {
-                return _dir.filename.startsWith(fullPath + '/') && _dir.filename.split('/').length === partsLength + 1
+                return (
+                    _dir.filename.startsWith(fullPath + "/") &&
+                    _dir.filename.split("/").length === partsLength + 1
+                )
             })
             .map((_dir: any) => {
                 let thumbnailPath = null
-                if (_dir.type === 'directory') {
-                    const imageFile = dirContents.find((file: any) =>
-                        file.filename.startsWith(_dir.filename + '/') &&
-                        file.filename.split('/').length === _dir.filename.split('/').length + 1 &&
-                        file.type === 'file' &&
-                        ['image/jpeg', 'image/png', 'image/webp', 'image/jpg', 'image/gif', 'image/avif'].includes(file.mime)
+                if (_dir.type === "directory") {
+                    const imageFile = dirContents.find(
+                        (file: any) =>
+                            file.filename.startsWith(_dir.filename + "/") &&
+                            file.filename.split("/").length ===
+                                _dir.filename.split("/").length + 1 &&
+                            file.type === "file" &&
+                            [
+                                "image/jpeg",
+                                "image/png",
+                                "image/webp",
+                                "image/jpg",
+                                "image/gif",
+                                "image/avif",
+                            ].includes(file.mime)
                     )
                     if (imageFile) {
-                        thumbnailPath = imageFile.filename.replace(root_dir, '')
+                        thumbnailPath = imageFile.filename.replace(root_dir, "")
                     }
                 }
                 if (!_dir.etag) {
@@ -102,22 +125,19 @@ export async function listContents(dir: string, deep: boolean = false, countItem
                 }
                 return {
                     ..._dir,
-                    filename: _dir.filename.replace(root_dir, ''),
-                    hasThumbnail: thumbnailPath
+                    filename: _dir.filename.replace(root_dir, ""),
+                    hasThumbnail: thumbnailPath,
                 }
             })
     }
-
 }
-
 
 export async function getFileContent(filename: string) {
     return await client.getFileContents(root_dir + filename)
 }
 
-
 export async function getPreview(filename: string) {
-    const fileContents = await getFileContent(root_dir + filename) as Buffer
+    const fileContents = (await getFileContent(root_dir + filename)) as Buffer
     const image = sharp(fileContents)
     const metadata = await image.metadata()
 
@@ -136,18 +156,17 @@ export async function getPreview(filename: string) {
     }
 }
 
-
 export async function getDownloadURL(filename: string) {
     return client.getFileDownloadLink(root_dir + filename)
 }
-
 
 export async function getTextContent(filename: string) {
     return await client.getFileContents(root_dir + filename, { format: "text" })
 }
 
-
-async function streamToBuffer(readableStream: NodeJS.ReadableStream): Promise<Buffer> {
+async function streamToBuffer(
+    readableStream: NodeJS.ReadableStream
+): Promise<Buffer> {
     const chunks: any[] = []
     for await (const chunk of readableStream) {
         chunks.push(chunk)
@@ -155,12 +174,10 @@ async function streamToBuffer(readableStream: NodeJS.ReadableStream): Promise<Bu
     return Buffer.concat(chunks)
 }
 
-
 export async function getVideoThumbnail(filename: string) {
-    const videoStream = client.createReadStream(
-        root_dir + filename,
-        { range: { start: 0, end: 1 } }
-    )
+    const videoStream = client.createReadStream(root_dir + filename, {
+        range: { start: 0, end: 1 },
+    })
 
     const videoContent = await streamToBuffer(videoStream)
 
@@ -172,15 +189,18 @@ export async function search(query: string) {
 
     if (fuzzy) {
         const basenames = dirContents.map((item: any) => item.basename)
-        return fuzz.extract(query, basenames, { scorer: fuzz.token_set_ratio })
+        return fuzz
+            .extract(query, basenames, { scorer: fuzz.token_set_ratio })
             .filter((item: any) => item[1] >= 80)
             .map((item: any) => {
-                return dirContents.find((element: any) => element.basename === item[0])
+                return dirContents.find(
+                    (element: any) => element.basename === item[0]
+                )
             })
             .map((item: any) => {
                 return {
                     ...item,
-                    filename: item.filename.replace(root_dir, '')
+                    filename: item.filename.replace(root_dir, ""),
                 }
             })
     } else {
@@ -191,7 +211,7 @@ export async function search(query: string) {
             .map((item: any) => {
                 return {
                     ...item,
-                    filename: item.filename.replace(root_dir, '')
+                    filename: item.filename.replace(root_dir, ""),
                 }
             })
     }
